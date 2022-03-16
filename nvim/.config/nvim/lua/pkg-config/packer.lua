b65280830b2cf67aa5fcd66ecad0188664e89fee
lamekino <79169033+lamekino@users.noreply.github.com>
2022-03-15 22:34:45 -0400

    removed duplicate

--- nvim/.config/nvim/lua/pkg-config/packer.lua
+++ nvim/.config/nvim/lua/pkg-config/packer.lua
@@ -45,7 +45,6 @@ return packer.startup(function(use)
     -- completion
     use { "hrsh7th/nvim-cmp" }
     use { "hrsh7th/cmp-nvim-lsp" }
-    use { 'hrsh7th/cmp-nvim-lsp' }
     use { 'hrsh7th/cmp-buffer' }
     use { 'hrsh7th/cmp-path' }
     use { 'hrsh7th/cmp-cmdline' }

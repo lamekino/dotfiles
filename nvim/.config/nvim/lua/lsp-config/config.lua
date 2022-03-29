d20b1a02c90f606eaafae07a3fc9c23f76bd0754
lamekino <79169033+lamekino@users.noreply.github.com>
2022-03-29 12:32:00 -0400

    Remove document highlight... for now

--- nvim/.config/nvim/lua/lsp-config/config.lua
+++ nvim/.config/nvim/lua/lsp-config/config.lua
@@ -32,22 +32,22 @@ M.setup = function()
     })
 end
 
-local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
+-- local function lsp_highlight_document(client)
+--   -- Set autocommands conditional on server_capabilities
 
-  -- when there is an lsp diagnostic, we report it when the cursour is over that
-  -- line
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-        augroup lsp_document_highlight
-            autocmd! * <buffer>
-            autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-            autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        augroup END]],
-      false
-    )
-  end
-end
+--   -- when there is an lsp diagnostic, we report it when the cursour is over that
+--   -- line
+--   if client.resolved_capabilities.document_highlight then
+--     vim.api.nvim_exec([[
+--         augroup lsp_document_highlight
+--             autocmd! * <buffer>
+--             autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+--             autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+--         augroup END]],
+--       false
+--     )
+--   end
+-- end
 
 local function lsp_keymaps(bufnr)
     local opts = { noremap = true, silent = true }
@@ -77,7 +77,7 @@ end
 
 M.on_attach = function(client, bufnr)
     lsp_keymaps(bufnr)
-    lsp_highlight_document(client)
+    -- lsp_highlight_document(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()

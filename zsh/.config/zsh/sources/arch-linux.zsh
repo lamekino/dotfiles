469dedf3fd654172db1ccb441d0cc61833fcab15
lamekino <79169033+lamekino@users.noreply.github.com>
2022-04-27 09:10:07 -0400

    refactor following last commit -- whoops

---
+++ zsh/.config/zsh/sources/arch-linux.zsh
@@ -0,0 +1,10 @@
+# https://fosskers.github.io/aura/usage.html
+# https://github.com/fosskers/aura
+if command -v aura &>/dev/null
+then
+    alias pacman="aura --hotedit --unsuppress"
+    alias aura="aura --hotedit --unsuppress"
+    export HAS_AURA=1
+fi
+
+alias hd="hexdump -C"

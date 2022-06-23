17e5805b1c489db029f0f4c56f74fdf659b6d98c
lamekino <zoey.j.mcbride@gmail.com>
2022-06-23 13:38:03 -0400

    added windows configs

---
+++ install.ps1
@@ -0,0 +1,30 @@
+$configFiles = @{
+    "nvim" = @{
+        src  = ".\nvim\.config\nvim"
+        dest = "$env:APPDATA\..\Local\nvim"
+    }
+    "powershell" = @{
+        src  = ".\powershell\.config\powershell\Microsoft.PowerShell_profile.ps1"
+        dest = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+    }
+    "alacritty" = @{
+        src  = ".\alacritty\.config\alacritty"
+        dest = "$env:APPDATA\alacritty"
+    }
+}
+
+$configFiles.Keys | ForEach-Object {
+    if (-Not (Test-Path -Path $configFiles[$_].dest)) {
+        New-Item -Type SymbolicLink `
+            -Path $configFiles[$_].dest `
+            -Target (Resolve-Path $configFiles[$_].src) `
+            | Out-Null
+
+        Write-Host -ForegroundColor Green `
+            ("Installed:`t{0}" -f $configFiles[$_].dest)
+    }
+    else {
+        Write-Host -ForegroundColor Red `
+            ("Skipping:`t{0}" -f $configFiles[$_].dest)
+    }
+}

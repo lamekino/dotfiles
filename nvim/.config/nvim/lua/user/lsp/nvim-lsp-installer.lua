e561bdd4b1ee33e0845c87a5921ac8725610f70e
lamekino <zoey.j.mcbride@gmail.com>
2022-07-08 15:47:01 -0400

    made loading from lsp/settings/<ls-name>.lua automatic

--- nvim/.config/nvim/lua/user/lsp/nvim-lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/nvim-lsp-installer.lua
@@ -1,26 +1,21 @@
--- https://github.com/williamboman/nvim-lsp-installer/
+local function set_lsp_opts(name, opts)
+    local okay, ext_opts = pcall(require, "user.lsp.settings." .. name)
 
--- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
--- or if the server is already installed).
+    -- if external config doesn't exist, use default values
+    -- or if the ext_opts is empty
+    if not okay or ext_opts == true then
+        return opts
+    end
+
+    return vim.tbl_deep_extend("force", ext_opts, opts);
+end
+
 require("nvim-lsp-installer").on_server_ready(function(server)
     local opts = {
         on_attach = require("user.lsp.config").on_attach,
         capablities = require("user.lsp.config").capabilities,
     }
 
-    -- (optional) Customize the options passed to the server
-    if server.name == "sumneko_lua" then
-        local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-        opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-    end
-
-    if server.name == "jdtls" then
-        local jdtls_opts = require("user.lsp.settings.jdtls")
-        opts = vim.tbl_deep_extend("force", jdtls_opts, opts)
-    end
-
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    opts = set_lsp_opts(server.name, opts)
     server:setup(opts)
 end)

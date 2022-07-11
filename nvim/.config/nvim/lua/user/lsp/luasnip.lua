cb0da67f9cfca9ff47bdfeda9222c828f51177cc
lamekino <zoey.j.mcbride@gmail.com>
2022-07-11 12:29:59 -0400

    remove broken stuff

--- nvim/.config/nvim/lua/user/lsp/luasnip.lua
+++ nvim/.config/nvim/lua/user/lsp/luasnip.lua
@@ -1,5 +1,4 @@
 local snip = require("luasnip")
-
 -- local types = require("luasnip.util.types")
 
 snip.config.set_config {
@@ -13,20 +12,3 @@ snip.snippets = {
         snip.parser.parse_snippet("lf", "local $1 = function ($2)\n    $0\nend"),
     },
 }
-
--- this is broken and idk why :(
-vim.keymap.set({ "i" }, "<C-k>", function ()
-    require("luasnip.extras.select_choice")()
-end, { silent = true })
-
-vim.keymap.set({ "i" }, "<C-j>", function ()
-    if snip.jumpable(-1) then
-        snip.jump(-1)
-    end
-end, { silent = true })
-
-vim.keymap.set({ "i" }, "<C-l>", function ()
-    if snip.choice_active() then
-        snip.change_choice(1)
-    end
-end, { silent = true })

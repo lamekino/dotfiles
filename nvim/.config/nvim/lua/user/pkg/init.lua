7f6404550317472a4e45e0b10c4a5bc4fee8792a
lamekino <zoey.j.mcbride@gmail.com>
2022-07-30 00:40:50 -0400

    moved small configs to init

--- nvim/.config/nvim/lua/user/pkg/init.lua
+++ nvim/.config/nvim/lua/user/pkg/init.lua
@@ -1,4 +1,31 @@
 require("user.pkg.packer")
 require("user.pkg.treesitter")
-require("user.pkg.colorizer")
 require("user.pkg.lualine")
+
+require("colorizer").setup {
+    "*",
+    css = { rgb_fn = true },
+}
+
+require("todo-comments").setup {
+    signs = false,
+    highlight = {
+        before = "",
+        keyword = "fg",
+        after = "fg",
+    }
+}
+
+require("diffview").setup {
+    use_icons = false
+}
+
+require("neogit").setup {
+    kind = "split",
+    disable_signs = true,
+    integrations = {
+        diffview = true
+    }
+}
+
+require("Comment").setup()

57c9625c3825597c2673420300f75fabd989ace5
lamekino <zoey.j.mcbride@gmail.com>
2022-07-30 00:37:36 -0400

    more hightlight tweaks

--- nvim/.config/nvim/lua/user/colors.lua
+++ nvim/.config/nvim/lua/user/colors.lua
@@ -1,15 +1,15 @@
 local M = {}
 
-local function hl(name, groups)
+local function hi(name, groups)
     vim.api.nvim_set_hl(0, name, groups)
 end
 
 local function lsp_colors()
     -- Setting up LSP number line diagnostics
-    hl("DiagnosticLineNrError", { bg = "#51202A", fg = "#FF0000" })
-    hl("DiagnosticLineNrWarn", { bg = "#51412A", fg = "#FFA501" })
-    hl("DiagnosticLineNrInfo", { bg = "#1E535D", fg = "#00FFFF" })
-    hl("DiagnosticLineNrHint", { bg = "#1E205D", fg = "#0000FF" })
+    hi("DiagnosticLineNrError", { bg = "#51202A", fg = "#FF0000" })
+    hi("DiagnosticLineNrWarn", { bg = "#51412A", fg = "#FFA501" })
+    hi("DiagnosticLineNrInfo", { bg = "#1E535D", fg = "#00FFFF" })
+    hi("DiagnosticLineNrHint", { bg = "#1E205D", fg = "#0000FF" })
 
     -- Set up the number line to be highlighted with a color instead of using
     -- text
@@ -27,9 +27,6 @@ end
 M.setup = function(colorscheme, mode)
     vim.o.background = mode
 
-    -- match the window background with normal
-    vim.o.winhl = "Normal:Normal"
-
     -- set autogroups for after the colorscheme is loaded
     vim.api.nvim_create_autocmd("Colorscheme", {
         desc     = "lsp line number indicators",
@@ -39,8 +36,25 @@ M.setup = function(colorscheme, mode)
     vim.api.nvim_create_autocmd("Colorscheme", {
         desc     = "colorscheme highlight tweaks",
         callback = function()
-            hl("Normal", { bg = "NONE" }) -- transparent bg
-            hl("Whitespace", { fg = "#353535" }) -- listchar colors
+            hi("Normal", { bg = "NONE" })
+            hi("FloatBorder", { fg = "#353535", bg = "NONE" })
+            hi("WinSeparator", { fg = "#353535", bg = "NONE" })
+            -- hi("Whitespace", { fg = "#353535", bg = "NONE" })
+
+            -- match the window background with normal
+            vim.o.winhl = "Normal:Normal,NormalNC:Normal"
+        end
+    })
+
+    vim.api.nvim_create_autocmd("ColorschemePre", {
+        desc     = "sets global colorscheme variables",
+        callback = function()
+            -- Tokyonight
+            vim.g.tokyonight_style       = "night"
+            vim.g.tokyonight_transparent = true
+
+            -- Catpuccin
+            vim.g.catppuccin_flavour = "mocha"
         end
     })
 

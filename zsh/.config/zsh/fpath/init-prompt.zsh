2c31ea39eec3c3fc6be6d70534b38220668028cb
lamekino <zoey.j.mcbride@gmail.com>
2022-08-16 01:19:12 -0400

    dynamic git prompt on unstaged changes

--- zsh/.config/zsh/fpath/init-prompt.zsh
+++ zsh/.config/zsh/fpath/init-prompt.zsh
@@ -4,16 +4,24 @@ function prompt-color() {
     echo -n "%F{$color}$string%f"
 }
 
+function git-prompt() {
+    local branch=$(git branch --show-current 2>/dev/null)
+    if [ -n "$branch" ]; then
+        local unstaged=$(git status --short 2>/dev/null)
+
+        if [ -z "$unstaged" ]; then
+            echo "$(prompt-color 10 "@$branch"):"
+        else
+            echo "$(prompt-color 11 "+$branch"):"
+        fi
+    fi
+}
+
 function precmd-prompt() {
     local dir="$(prompt-color 12 '%~')"
     local err="$(prompt-color 1 '%?')"
     # local job="$(prompt-color 3 '%j')"
-
-    # get git branch name
-    local branch=$(git branch --show-current 2>/dev/null)
-    if [ -n "$branch" ]; then
-        local git="$(prompt-color 10 "*$branch"):"
-    fi
+    local git="$(git-prompt)"
 
     # _PROMPT="%(?..${err} )%(1j.${job} .)${git}${dir}"
     _PROMPT="%(?..${err} )${git}${dir}"

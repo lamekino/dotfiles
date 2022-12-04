467cecd9d0648e80b371aa30acc18f017024098d
lamekino <zoey.j.mcbride@gmail.com>
2022-12-04 13:25:55 -0500

    doesn't make a new zshenv if it exists, added nvim configuration

--- install.sh
+++ install.sh
@@ -13,6 +13,19 @@ for dot in ${dotfiles}; do
     echo "installing: $dot"
     stow -t "$HOME" "$dot"
 done
-echo ". ~/.config/zsh/.zshenv" > ~/.zshenv
-touch ~/.config/zsh/sources
 
+! [ -f ~/.zshenv ] \
+    && echo ". ~/.config/zsh/.zshenv" > ~/.zshenv
+
+[ -z "$FRESH_INSTALL" ] && exit
+
+# configure neovim
+if ! [ -f ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+    nvim -u NONE -c \
+        "lua dofile('./nvim/.config/nvim/plugin/packer.lua'); \
+        require('packer').sync(); \
+        print('finished')"
+fi

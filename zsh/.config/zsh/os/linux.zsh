028c6712c317e736149b6f3244224f550da94252
lamekino <zoey.j.mcbride@gmail.com>
2022-12-17 13:24:04 -0500

    ls more sensible sorting

--- zsh/.config/zsh/os/linux.zsh
+++ zsh/.config/zsh/os/linux.zsh
@@ -1,9 +1,9 @@
 alias grep='grep --color=auto'
 alias lablk="lsblk -o name,label,size,ro,type,mountpoint,uuid"
-alias ls='ls -pk --color=auto --group-directories-first'
-alias ll='ls -pklh --color=auto --group-directories-first'
-alias la='ls -pkah --color=auto --group-directories-first'
-alias lla='ls -pkalh --color=auto --group-directories-first'
+alias ls='LC_ALL=C ls -pk --color=auto --group-directories-first'
+alias ll='LC_ALL=C ls -pklh --color=auto --group-directories-first'
+alias la='LC_ALL=C ls -pkah --color=auto --group-directories-first'
+alias lla='LC_ALL=C ls -pkalh --color=auto --group-directories-first'
 alias diff='diff --color=always'
 alias feh='feh -x --scale-down'
 alias feh-svg="feh --magick-timeout 1 $1"

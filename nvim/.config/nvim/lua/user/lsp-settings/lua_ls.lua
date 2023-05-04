d0d683c4ad544b0acc89f6b399dd089155220288
lamekino <zoey.j.mcbride@gmail.com>
2023-05-04 17:16:45 -0400

    match new lsp name

--- nvim/.config/nvim/lua/user/lsp-settings/lua_ls.lua
+++ nvim/.config/nvim/lua/user/lsp-settings/lua_ls.lua
@@ -3,12 +3,19 @@ local M = {}
 M.settings = {
     Lua = {
         diagnostics = {
-            globals = { "vim", "NONE" },
+            enable = true,
+            globals = {
+                -- nvim
+                "vim", "NONE",
+                -- awesome wm
+                "awesome", "client", "root"
+            },
         },
         workspace = {
             library = {
                 [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                 [vim.fn.stdpath("config") .. "/lua"] = true,
+                ["/usr/share/awesome/lib"] = true
             },
         },
     },

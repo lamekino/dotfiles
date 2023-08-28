633b29a0ff531c9a248203f8cf6b399eb4628714
lamekino <zoey.j.mcbride@gmail.com>
2023-08-28 15:29:23 -0400

    autocmd to change directory to git root on vim startup

--- nvim/.config/nvim/after/plugin/neogit.lua
+++ nvim/.config/nvim/after/plugin/neogit.lua
@@ -8,3 +8,23 @@ neogit.setup {
         diffview = true
     }
 }
+
+local neogit_group = vim.api.nvim_create_augroup("Neogit", {
+    clear = true
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    desc = "set the cwd to the git project's root",
+    group = neogit_group,
+    callback = function()
+        local git_root = neogit.cli.git_root()
+        if (string.len(git_root) <= 0) then
+            return
+        end
+
+        if (string.len(vim.fn.chdir(git_root)) <= 0) then
+            vim.api.nvim_err_writeln("error: could not change to git root: "
+                .. git_root)
+        end
+    end
+})

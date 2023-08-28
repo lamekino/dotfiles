daa34788fad19d24600646d1dc290aa7d41272a0
lamekino <zoey.j.mcbride@gmail.com>
2023-08-28 15:27:01 -0400

    don't highlight colornames please so annoying

--- nvim/.config/nvim/after/plugin/colorizer.lua
+++ nvim/.config/nvim/after/plugin/colorizer.lua
@@ -1,7 +1,7 @@
 local okay, colorizer = pcall(require, "colorizer")
 if not okay then return end
 
-colorizer.setup {
-    "*",
-    css = { rgb_fn = true },
-}
+colorizer.setup(nil, {
+    '*',
+    names = false,
+})

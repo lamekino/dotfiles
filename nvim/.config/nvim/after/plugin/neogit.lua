39d091221aae9b125ae176049b0327d43db4c7db
lamekino <zoey.j.mcbride@gmail.com>
2024-02-18 12:41:38 -0500

    fix following deprecation

--- nvim/.config/nvim/after/plugin/neogit.lua
+++ nvim/.config/nvim/after/plugin/neogit.lua
@@ -9,22 +9,28 @@ neogit.setup {
     }
 }
 
+local function cd_git_root()
+    local git_root = neogit.cli.git_root_of_cwd()
+    if git_root == nil or string.len(git_root) <= 0 then
+        return
+    end
+
+    if string.len(vim.fn.chdir(git_root)) <= 0 then
+        vim.api.nvim_err_writeln("error: could not change to git root: "
+            .. git_root)
+    end
+end
+
 local neogit_group = vim.api.nvim_create_augroup("Neogit", {
     clear = true
 })
 
 vim.api.nvim_create_autocmd("VimEnter", {
+    callback = cd_git_root,
     desc = "set the cwd to the git project's root",
     group = neogit_group,
-    callback = function()
-        local git_root = neogit.cli.git_root()
-        if (string.len(git_root) <= 0) then
-            return
-        end
+})
 
-        if (string.len(vim.fn.chdir(git_root)) <= 0) then
-            vim.api.nvim_err_writeln("error: could not change to git root: "
-                .. git_root)
-        end
-    end
+vim.api.nvim_create_user_command("GitRootCd", cd_git_root, {
+    bang = true
 })

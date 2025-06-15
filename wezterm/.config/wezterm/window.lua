5ba0196a3354dfac7871e593706c8643472df3ca
z <z@macbook.local>
2025-06-15 13:24:51 -0400

    set default tab title much faster

--- wezterm/.config/wezterm/window.lua
+++ wezterm/.config/wezterm/window.lua
@@ -1,42 +1,41 @@
-local init_tab_title = "untitled" -- empty string = running program
+local window_title = "WezTerm"
 
 local function my_events(wt)
     return {
         ["format-window-title"] =
             (function()
-                return "WezTerm"
+                return window_title
             end),
-        ["gui-startup"] =
-            (function(cmd)
-                local env = cmd or {}
-                local _, pane, window = wt.mux.spawn_window(env)
-
-                -- skip set title prompt (zshrc)
-                if init_tab_title ~= "" then
-                    pane:send_text(string.format("'%s'", init_tab_title))
-                end
-                pane:send_text("\n")
-            end)
+        ["format-tab-title"] = -- (tab, tabs, panes, config, hover, max_width)
+            (function(tab, ...)
+                return string.format(
+                    " %d: %s ",
+                    tab.tab_index + 1, tab.tab_title
+                )
+            end),
     }
 end
 
 return function(cfg, wt)
-    cfg.tab_bar_at_bottom = true
-    cfg.use_fancy_tab_bar = false
+    -- window
+    local padding = 8
+
     cfg.window_close_confirmation = 'NeverPrompt'
     cfg.window_decorations = 'RESIZE'
-    -- cfg.hide_tab_bar_if_only_one_tab = true
-    -- cfg.enable_tab_bar = false
-    -- cfg.enable_wayland = false
-
-    local pad = 8
     cfg.window_padding = {
-        left = pad,
-        right = pad,
-        top = pad,
-        bottom = pad
+        left = padding,
+        right = padding,
+        top = padding,
+        bottom = padding
     }
 
+    -- tabs
+    cfg.enable_tab_bar = true
+    cfg.use_fancy_tab_bar = false
+    cfg.tab_bar_at_bottom = true
+    cfg.hide_tab_bar_if_only_one_tab = false
+
+    -- set events
     for evname, callback in pairs(my_events(wt)) do
         wt.on(evname, callback)
     end
--- zsh/.config/zsh/util/new-tab.zsh
+++ zsh/.config/zsh/util/new-tab.zsh
@@ -1,34 +1,33 @@
 local terminal="wezterm-gui"
 local titlecmd="wezterm cli set-tab-title"
 
-local prompttext="tab title"
+local set_title= # skips prompting if set
+local fallback_title="untitled"
+
+local prompttext="tab title?"
 local promptcolor=140
 
-local default_title= # skips prompting if set
+local has_terminal=0
+local shell_parent="$(ps -o ppid\= $$)"
 
-function set-tab-title() {
-    local set_title="$1"
-    local has_title=0
-    local ppid="$(ps -o ppid\= $$)"
+# set the default if ctrl-c is pressed
+trap "$titlecmd '$fallback_title'; trap -; return" INT TERM
 
-    # grep for the terminal in the ppid's command name
-    if ps -o comm -p "$ppid" | tail -n +2 | grep -q "$terminal$"; then
-        has_title=1
-    fi
-
-    if (( has_title )) && [ -z "$set_title" ]; then
-        printf "\x1b[38;5;%sm%s\x1b[0m > " "$promptcolor" "$prompttext"
-        read -r set_title
-        clear
-    fi
-
-    if (( has_title )) && [ -n "$set_title" ]; then
-        eval "$titlecmd "$set_title""
-    fi
-}
-
-if [ -z "$default_title" ]; then
-    set-tab-title "untitled"
+# grep for the terminal name in the ppid's command name
+if ps -o comm -p "$shell_parent" | tail -n +2 | grep -q "$terminal$"; then
+    has_terminal=1
 fi
 
-set-tab-title "$default_title"
+if (( has_terminal )) && [ -z "$set_title" ]; then
+    printf "\x1b[38;5;%sm%s\x1b[0m > " "$promptcolor" "$prompttext"
+    read -r set_title
+    clear
+fi
+
+if (( has_terminal )); then
+    # set default if ctrl-d is pressed
+    [ -z "$set_title" ] && set_title="$fallback_title"
+    eval "$titlecmd '$set_title'"
+fi
+
+trap - # reset trap handlers

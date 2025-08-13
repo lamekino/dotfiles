ac993343b92b735410c0b7f313b1a5ce5fd53bb0
lamekino <79169033+lamekino@users.noreply.github.com>
2025-08-13 13:14:38 -0400

    more copy/paste keys

--- user/wezterm/.config/wezterm/keyboard.lua
+++ user/wezterm/.config/wezterm/keyboard.lua
@@ -1,29 +1,39 @@
 function M(cfg, wt)
     cfg.keys = {
         { -- make <C-[> to send escape code (Esc), like older terminals
-            key = '[',
-            mods = 'CTRL',
+            key = "[",
+            mods = "CTRL",
             action = wt.action.SendKey({ key = "Escape" })
         },
         { -- reload config
-            key = 'r',
-            mods = 'CMD|SHIFT',
+            key = "r",
+            mods = "CMD|SHIFT",
             action = wt.action.ReloadConfiguration,
         },
         { -- disable annoying behavior
-            key = ' ',
-            mods = 'SHIFT',
+            key = " ",
+            mods = "SHIFT",
             action = wt.action.Nop
+        },
+        { -- alt+v paste
+            key = "v",
+            mods = "ALT",
+            action = wt.action.PasteFrom("Clipboard")
+        },
+        { -- alt+c copy
+            key = "c",
+            mods = "ALT",
+            action = wt.action.CopyTo("Clipboard")
         },
     }
 
     -- macos cmdkeys -> altkeys, im not breaking this habit
     for number_key = 0, 9 do
         table.insert(cfg.keys, {
-            mods = 'SUPER',
+            mods = "SUPER",
             key = tostring(number_key),
             action = wt.action.SendKey({
-                mods = 'ALT',
+                mods = "ALT",
                 key = tostring(number_key)
             })
         })

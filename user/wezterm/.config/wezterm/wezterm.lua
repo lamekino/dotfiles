44a0dd2a3cd0eab16a97b94295e8238746538f1e
lamekino <79169033+lamekino@users.noreply.github.com>
2025-09-03 15:29:13 -0400

    bad at git

--- user/wezterm/.config/wezterm/wezterm.lua
+++ user/wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wt = require("wezterm")
 local my_cfg = {}
 
 for _, mod in ipairs({
-    "init", "events", "keyboard", "mouse", "ui", "theme", "fonts"
+    "initial", "events", "keyboard", "mouse", "ui", "theme", "fonts"
 }) do
     require(mod)(my_cfg, wt)
 end
